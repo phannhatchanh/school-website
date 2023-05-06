506ca14c5455a259516d43b5d8965fdab96ccb26
Phan Nhat Chanh <phannhatchanh@gmail.com>
2023-05-06 15:19:50 +0700

    use `.env`

--- sanity/sanity.cli.js
+++ sanity/sanity.cli.js
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'vzlac2zd',
+    projectId: process.env.SANITY_PROJECT_ID,
     dataset: 'production'
   }
 })
